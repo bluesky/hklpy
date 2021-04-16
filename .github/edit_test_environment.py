612a89f16e4d21e233ed5494656d0f0e8c950fd0
Pete R Jemian <prjemian@gmail.com>
2021-04-16 12:26:46 -0500

    MNT #111 apply conda & pip installs

--- .github/edit_test_environment.py
+++ .github/edit_test_environment.py
@@ -19,10 +19,11 @@ CONFIG_FILE = os.path.join(PATH, "environment.yml")
 def main():
     # get command-line argument(s)
     env_name = sys.argv[1]  # hklpy-test-${{ matrix.python-version }}
+    py_ver = env_name.split("-")[-1]  # BIG assumption
 
     # read the environment file
     with open(CONFIG_FILE, "r") as f:
-        full_config = yaml.load(f, Loader=yaml.FullLoader)
+        full_config = yaml.safe_load(f)
 
     # set the specific environment name
     full_config["name"] = env_name
@@ -31,6 +32,11 @@ def main():
     pip_requirements = full_config["dependencies"][-1]["pip"]
     # remove them from the conda environment
     full_config["dependencies"] = full_config["dependencies"][:-1]
+    # require specific python version
+    import pprint
+    pprint.pprint(full_config)
+    full_config["dependencies"].pop(0)
+    full_config["dependencies"].insert(0, f"python ={py_ver}")
 
     # write a new environment YAML file without the pip requirements
     with open(os.path.join(PATH, "conda_env.yml"), "w") as f:
