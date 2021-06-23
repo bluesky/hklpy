1ec17e63f8f2f84170054a21594d6ab5e75ccaa4
Pete R Jemian <prjemian@gmail.com>
2021-06-23 12:51:31 -0500

    TST #160

--- hkl/tests/test_util.py
+++ hkl/tests/test_util.py
@@ -1,34 +1,73 @@
 import gi
 
 gi.require_version("Hkl", "5.0")
-# import pytest
+import pytest
 
-# FIXME: comment remainder for now until #185 is resolved
-
-
-def test_true():
-    assert True
-    # FIXME: remove when #185 is solved.
+import hkl.util
+from packaging import version
 
 
-# from hkl.util import get_package_info, _package_info
+NO_SUCH_PACKAGE_NAME = "no-such-package"
 
 
-# def test__package_info_states():
-#     assert _package_info is None
-#     get_package_info()
-#     assert _package_info is not None
+def test__package_info_states():
+    assert hkl.util._package_info is None
+    hkl.util.get_package_info("hkl")
+    assert hkl.util._package_info is not None
 
 
-# def test_get_package_info():
-#     v = get_package_info("hkl")
-#     assert v.startswith("5.0.0.")
-#     assert get_package_info("no-such-package") is None
-#     # TODO: assert get_package_info("hklpy")["version"] >= "0.3.16"
+@pytest.mark.parametrize(
+    "package_name, minimum_version",
+    [
+        ("bluesky", "1.6"),
+        ("gobject-introspection", "1.68.0"),
+        ("hkl", "5.0.0"),
+        ("hklpy", "0.3.16"),
+        (NO_SUCH_PACKAGE_NAME, "---"),
+        ("ophyd", "1.6"),
+    ],
+)
+def test_get_package_info(package_name, minimum_version):
+    v = hkl.util.get_package_info(package_name)
+    if v is None:
+        assert package_name == NO_SUCH_PACKAGE_NAME
+    else:
+        assert "version" in v
+        v_string = v.get("version", "unknown")
+
+        if package_name == "hkl":
+            assert v_string.startswith("5.0.0.")
+
+        v_package = version.parse(v_string)
+        assert v_package >= version.parse(minimum_version)
 
 
-# def test_software_versions_default():
-#     v = software_versions()
-#     assert v["hkl"].startswith("5.0.0.")
-#     # TODO: assert v.get("no-such-package") is None
-#     # TODO: assert v.get("hklpy") >= "0.3.16"
+@pytest.mark.parametrize(
+    "case",
+    [
+        (None),
+        ([]),
+        (""),
+    ],
+)
+def test_software_versions_default_list(case):
+    v = hkl.util.software_versions(case)
+    assert isinstance(v, dict)
+    expected = sorted(hkl.util.DEFAULT_PACKAGE_LIST)
+    assert sorted(v.keys()) == expected
+
+
+@pytest.mark.parametrize(
+    "package_name, minimum_version",
+    [
+        ("bluesky", "1.6"),
+        ("gobject-introspection", "1.68.0"),
+        ("hkl", "5.0.0"),
+        ("hklpy", "0.3.16"),
+        (NO_SUCH_PACKAGE_NAME, "---"),
+        ("ophyd", "1.6"),
+    ],
+)
+def test_software_versions_items(package_name, minimum_version):
+    v = hkl.util.software_versions([package_name,])
+
