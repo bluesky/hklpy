8981cb91b3adc943a1fdfa4a380838225b3a548b
Pete R Jemian <prjemian@gmail.com>
2021-09-15 13:09:08 -0500

    MNT #202 use README.md

--- setup.py
+++ setup.py
@@ -3,17 +3,18 @@ from setuptools import setup, find_packages
 import versioneer
 
 
-description = (
-    "Controls for using diffractometers within the"
-    " Bluesky Framework: https://blueskyproject.io"
-)
+desc = "Controls for using diffractometers within the Bluesky Framework"
+desc += ": https://blueskyproject.io"
+with open("README.md", "r") as f:
+    long_desc = f.read()
 
 setup(
     name="hklpy",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    description=description,
-    long_description=description,
+    description=desc,
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
     maintainer="prjemian",
     maintainer_email="prjemian+hklpy@gmail.com",
     url="https://github.com/bluesky/hklpy",
