d666c941369123b3f64442e4a3cdef1eacddc48b
Pete R Jemian <prjemian@gmail.com>
2021-09-15 13:03:03 -0500

    DOC #202

--- setup.py
+++ setup.py
@@ -3,11 +3,17 @@ from setuptools import setup, find_packages
 import versioneer
 
 
+description = (
+    "Controls for using diffractometers within the"
+    " Bluesky Framework: https://blueskyproject.io"
+)
+
 setup(
     name="hklpy",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    description="Controls for using diffractometers within the Bluesky Framework: https://blueskyproject.io",
+    description=description,
+    long_description=description,
     maintainer="prjemian",
     maintainer_email="prjemian+hklpy@gmail.com",
     url="https://github.com/bluesky/hklpy",
