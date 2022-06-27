35e8999ae7de23c3c00a32608dd0955232e52844
Pete R Jemian <prjemian@gmail.com>
2022-06-27 16:24:32 -0500

    ENH #189 #143 basic class and repr

---
+++ hkl/reflect.py
@@ -0,0 +1,49 @@
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+class Reflection:
+    """
+    A crystal orientation reflection.
+    """
+
+    wavelength = None
+    pseudos = []
+    reals = []
+
+    def __init__(self, pseudos, reals, wavelength) -> None:
+        # fmt: off
+        # confirm pseudos and reals are lists of appropriate objects
+        for var in (pseudos, reals):
+            for obj in var:
+                for attr in "attr_name position".split():
+                    if not hasattr(obj, attr):
+                        raise AttributeError(f"{var}, '{obj}.{attr}' not found")
+        # fmt: on
+
+        self.pseudos = pseudos
+        self.reals = reals
+        self.wavelength = wavelength
+
+    def _repr_info(self):
+        p = [
+            f"{obj.attr_name}={obj.position}"
+            for obj in self.pseudos
+        ]
+        r = [
+            f"{obj.attr_name}={obj.position}"
+            for obj in self.reals
+        ]
+
+        s = [
+            f"({', '.join(p)})",
+            f"({', '.join(r)})",
+            f"wavelength={self.wavelength!r}",
+        ]
+
+        return s
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({', '.join(self._repr_info())})"
