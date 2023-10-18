5d7c943b40657320c9a37e76c4084aa8affabc1c
Pete R Jemian <prjemian@gmail.com>
2023-10-18 09:46:09 -0500

    DOC #267 make copyright years consistent

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,7 +63,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "hklpy"
-copyright = "2014-2021, Brookhaven National Laboratory"
+copyright = "2014-2023, Brookhaven National Laboratory"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
