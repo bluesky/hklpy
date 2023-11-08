45c98c49a62e81849ce6164394b12b38cb4279e8
Pete R Jemian <prjemian@gmail.com>
2023-11-08 13:32:03 -0600

    TST #289 test for the reported problem

--- hkl/tests/__init__.py
+++ hkl/tests/__init__.py
@@ -11,22 +11,22 @@ TWO_PI = 2 * numpy.pi
 
 
 def new_sample(diffractometer, name, lattice):
-    diffractometer.calc.new_sample(name, lattice=lattice)
+    return diffractometer.calc.new_sample(name, lattice=lattice)
 
 
 def sample_kryptonite(diffractometer):
     triclinic = new_lattice(4, 5, 6, 75, 85, 95)
-    new_sample(diffractometer, "kryptonite", lattice=triclinic)
+    return new_sample(diffractometer, "kryptonite", lattice=triclinic)
 
 
 def sample_silicon(diffractometer):
     from .. import SI_LATTICE_PARAMETER
 
     cubic = new_lattice(SI_LATTICE_PARAMETER)
-    new_sample(diffractometer, "silicon", lattice=cubic)
+    return new_sample(diffractometer, "silicon", lattice=cubic)
 
 
 def sample_vibranium(diffractometer):
     a0 = TWO_PI
     cubic = new_lattice(a0)
-    new_sample(diffractometer, "vibranium", lattice=cubic)
+    return new_sample(diffractometer, "vibranium", lattice=cubic)
